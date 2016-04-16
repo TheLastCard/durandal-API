14d4d69c4e4b7db536660716b2daef34213e9fb1
Walter Kjell Dahl <walter@ultraclearance.com>
2016-04-16 23:41:56 +0200

    Update API to have a Product controller. Added migrations with a few seed products. Working with some problems accessing the database

--- Durandal-API/Migrations/Configuration.cs
+++ Durandal-API/Migrations/Configuration.cs
@@ -1,18 +1,19 @@
 namespace Durandal_API.Migrations
 {
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Durandal_API.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Durandal_API.Models.DurandalDB>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Durandal_API.Models.ApplicationDbContext context)
+        protected override void Seed(Durandal_API.Models.DurandalDB context)
         {
             //  This method will be called after migrating to the latest version.
 
@@ -25,7 +26,14 @@ namespace Durandal_API.Migrations
             //      new Person { FullName = "Brice Lambson" },
             //      new Person { FullName = "Rowan Miller" }
             //    );
-            //
+
+            context.Products.AddOrUpdate(
+                new ProductModel() { Name = "Webcam", Price = 499, Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium fringilla nisi a egestas. Aliquam et augue euismod neque scelerisque malesuada ut nec sem. Sed hendrerit porta leo, sit amet mollis odio dapibus ut. Etiam efficitur posuere mi eu feugiat." },
+                new ProductModel() { Name = "Raspberry PI 3", Price = 749, Description = "Vivamus dignissim nulla eget mauris molestie elementum. Integer consequat ligula scelerisque velit feugiat, at accumsan massa tincidunt." },
+                new ProductModel() { Name = "Headphones", Price = 249, Description = "Pellentesque id turpis iaculis, convallis nisl vel, ullamcorper leo. Sed eu arcu sit amet ex consectetur elementum hendrerit nec nulla. Nullam varius consequat purus in tempor." },
+                new ProductModel() { Name = "Samsung 48\" Smart TV", Price = 299, Description = "Vestibulum sodales luctus eros, ac blandit tortor accumsan sit amet. Mauris vestibulum leo eros. Aenean ut ligula malesuada, convallis nunc malesuada, faucibus nisi." }
+            );
+            context.SaveChanges();
         }
     }
 }
--- Durandal-API/Models/DurandalDB.cs
+++ Durandal-API/Models/DurandalDB.cs
@@ -8,12 +8,35 @@ namespace Durandal_API.Models
 {
     public class DurandalDB : DbContext
     {
-        public DurandalDB() : base("DefaultConnection")
-        {
-            Database.SetInitializer<DurandalDB>(new ProductDBInitializer());
-        }
 
         public DbSet<ProductModel> Products { get; set; }
 
+        public void Seed(DurandalDB Context)
+        {
+#if DEBUG
+            // Create my debug (testing) objects here
+            Context.Products.Add(new ProductModel() { Name = "Webcam", Price = 499, Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pretium fringilla nisi a egestas. Aliquam et augue euismod neque scelerisque malesuada ut nec sem. Sed hendrerit porta leo, sit amet mollis odio dapibus ut. Etiam efficitur posuere mi eu feugiat." });
+            Context.Products.Add(new ProductModel() { Name = "Raspberry PI 3", Price = 749, Description = "Vivamus dignissim nulla eget mauris molestie elementum. Integer consequat ligula scelerisque velit feugiat, at accumsan massa tincidunt." });
+            Context.Products.Add(new ProductModel() { Name = "Headphones", Price = 249, Description = "Pellentesque id turpis iaculis, convallis nisl vel, ullamcorper leo. Sed eu arcu sit amet ex consectetur elementum hendrerit nec nulla. Nullam varius consequat purus in tempor." });
+            Context.Products.Add(new ProductModel() { Name = "Samsung 48\" Smart TV", Price = 299, Description = "Vestibulum sodales luctus eros, ac blandit tortor accumsan sit amet. Mauris vestibulum leo eros. Aenean ut ligula malesuada, convallis nunc malesuada, faucibus nisi." });
+            Context.SaveChanges();
+#endif
+        }
+
+        public class DropCreateIfChangeInitializer : DropCreateDatabaseIfModelChanges<DurandalDB>
+        {
+            protected override void Seed(DurandalDB context)
+            {
+                context.Seed(context);
+                base.Seed(context);
+            }
+        }
+
+        static DurandalDB()
+        {
+#if DEBUG
+            Database.SetInitializer<DurandalDB>(new DropCreateIfChangeInitializer());
+#endif
+        }
     }
 }
